ac154e14c734a4f733472d7fde12f8b7fae95920
jarppe <jarppe@metosin.fi>
2015-04-28 16:33:25 +0300

    finally works

--- src/clj/bootbook/books.clj
+++ src/clj/bootbook/books.clj
@@ -8,30 +8,32 @@
                             (map name langs)
                             (map name authors)))))
 
-(def books (map with-index [{:title   "The Joy of Clojure"
-                             :langs   #{:clojure}
-                             :pages   328
-                             :authors [:fogus :houser]}
-                            {:title   "Erlang Programming"
-                             :langs   #{:erlang}
-                             :pages   470
-                             :authors [:cesarini :thompson]}
-                            {:title   "Clojure Data Analysis Cookbook"
-                             :langs   #{:clojure}
-                             :pages   326
-                             :authors [:rochester]}
-                            {:title   "Programming Concurrency on the JVM"
-                             :langs   #{:java :ruby :groovy :scala :clojure}
-                             :pages   270
-                             :authors [:venkat]}
-                            {:title   "The Little Schemer"
-                             :langs   #{:scheme}
-                             :pages   196
-                             :authors [:friedman :felleisen :sussman]}
-                            {:title   "Types and Programming Languages"
-                             :langs   #{:haskel :java :fortran}
-                             :pages   623
-                             :authors [:abelson :sussman :jsussman]}]))
+(def books (atom (map with-index [{:title   "The Joy of Clojure"
+                                   :langs   #{:clojure}
+                                   :pages   328
+                                   :authors [:fogus :houser]
+                                   :read?   true}
+                                  {:title   "Erlang Programming"
+                                   :langs   #{:erlang}
+                                   :pages   470
+                                   :authors [:cesarini :thompson]}
+                                  {:title   "Clojure Data Analysis Cookbook"
+                                   :langs   #{:clojure}
+                                   :pages   326
+                                   :authors [:rochester]}
+                                  {:title   "Programming Concurrency on the JVM"
+                                   :langs   #{:java :ruby :groovy :scala :clojure}
+                                   :pages   270
+                                   :authors [:venkat]
+                                   :read?   true}
+                                  {:title   "The Little Schemer"
+                                   :langs   #{:scheme}
+                                   :pages   196
+                                   :authors [:friedman :felleisen :sussman]}
+                                  {:title   "Types and Programming Languages"
+                                   :langs   #{:haskel :java :fortran}
+                                   :pages   623
+                                   :authors [:abelson :sussman :jsussman]}])))
 
 (defn ->finder [search]
   (let [pattern (re-pattern (str "^" search))
@@ -40,7 +42,25 @@
       (some match? index))))
 
 (defn search [request]
-  (println "search:" (some-> request :params :search string/lower-case ->finder (filter books)))
-  (ok (some-> request :params :search string/lower-case ->finder (filter books))))
+  (ok (some-> request
+              :params
+              :search
+              string/lower-case
+              ->finder
+              (filter @books))))
 
-(def routes {:get search})
+
+(defn set-when-title [target-title read?]
+  (fn [{:keys [title] :as book}]
+    (if (= title target-title)
+      (assoc book :read? read?)
+      book)))
+
+(defn set-as-read [request]
+  (let [title (-> request :params :title)
+        read? (-> request :params :read? Boolean/parseBoolean)]
+    (ok (swap! books (fn [books]
+                       (map (set-when-title title read?) books))))))
+
+(def routes {:get    search
+             "/read" {:post set-as-read}})
--- src/cljs/bootbook/ui/main.cljs
+++ src/cljs/bootbook/ui/main.cljs
@@ -5,12 +5,13 @@
             [cljs.core.async :refer [<!] :as a]
             [cljs-http.client :as http]))
 
+(defonce books (atom nil))                                  ; FIXME!!!
 
 (defn nav-bar []
   [:div.nav-bar
    [:ul
     [:li [:a.brand {:href "#"}
-          [:fa.fa.fa-book]
+          [:i.fa.fa-book]
           " BootBook"]]]])
 
 (defn search-books [search-value books]
@@ -21,24 +22,44 @@
          :body
          (reset! books))))
 
+(defonce search-value (atom nil))                           ; FIXME
+
 (defn search-form [books]
-  (let [search-value (atom nil)]
+  (let [search-value-FIXME (atom nil)]
     (fn []
-      [:form.search {:on-submit (fn [e] (.preventDefault e) (search-books @search-value books))}
+      [:form.search {:on-submit
+                     (fn [e]
+                       (.preventDefault e)
+                       (search-books @search-value books))}
        [:input {:type        "text"
                 :value       @search-value
-                :on-change   (fn [e] (reset! search-value (-> e .-target .-value)))
+                :on-change   (fn [e]
+                               (reset! search-value (-> e .-target .-value)))
                 :autofocus   true
                 :placeholder "Search..."}]])))
 
+(defn set-as-read [title read?]
+  (js/console.log "change:" title read?)
+  (go
+    (->> {:form-params {:title title, :read? read?}}
+         (http/post "/api/books/read")
+         <!
+         :body
+         (reset! books))))
 
-(defn book-view [{:keys [title langs authors]}]
+(defn book-view [{:keys [title langs authors read?]}]
   [:div.book {:key title}
    [:div
     [:span.title title]
     [:span.langs (for [lang langs]
                    [:span {:key lang} (-> lang name string/capitalize)])]]
-   [:div.authos (string/join ", " (map (comp string/capitalize name) authors))]])
+   [:div.authos (string/join ", " (map (comp string/capitalize name) authors))]
+   [:label
+    [:input {:type      "checkbox"
+             :checked   read?
+             :on-change (fn [e]
+                          (set-as-read title (-> e .-target .-checked)))}]
+    "Read?"]])
 
 (defn book-list [books]
   [:div.books
@@ -46,9 +67,9 @@
      (map book-view book-list))])
 
 (defn main-view []
-  (let [books (atom nil)]
+  (let [books-FIXME (atom nil)]
     (fn []
-      [:div
+      [:div.application
        [nav-bar]
        [search-form books]
        [book-list books]])))
